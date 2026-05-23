77c3294e9ed456a3e731cb6727dea56140af3518
Nathan Romero <91339715+Nathan-Romero@users.noreply.github.com>
2026-05-22 20:26:10 -0400

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.cpp
@@ -0,0 +1,35 @@
+class LRUCache {
+    unordered_map<int, pair<int, list<int>::iterator>> cache;
+    list<int> order;
+    int cap;
+
+public:
+    LRUCache(int capacity) { cap = capacity; }
+
+    int get(int key) {
+        if (!cache.contains(key))
+            return -1;
+        order.erase(cache[key].second);
+        order.push_back(key);
+        cache[key].second = --order.end();
+        return cache[key].first;
+    }
+
+    void put(int key, int value) {
+        if (cache.contains(key)) {
+            order.erase(cache[key].second);
+        } else if (cache.size() == cap) {
+            cache.erase(order.front());
+            order.pop_front();
+        }
+        order.push_back(key);
+        cache[key] = {value, --order.end()};
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
