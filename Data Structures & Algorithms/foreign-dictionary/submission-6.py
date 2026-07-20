a3ad53b8c178ee52733b29143587d91954977cef
Nathan Romero <91339715+Nathan-Romero@users.noreply.github.com>
2026-07-20 11:27:20 -0400

    Add: foreign-dictionary - submission-6

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        adj = {c: set() for w in words for c in w}
+        indeg = {c: 0 for c in adj}
+        for a, b in pairwise(words):
+            if a.startswith(b) and a != b:
+                return ""
+            for a, b in zip(a, b):
+                if a != b:
+                    if b not in adj[a]:
+                        adj[a].add(b)
+                        indeg[b] += 1
+                    break
+
+        q, res = deque([c for c, d in indeg.items() if not d]), []
+        while q:
+            for nei in adj[c := q.popleft()]:
+                indeg[nei] -= 1
+                if not indeg[nei]:
+                    q.append(nei)
+            res.append(c)
+
+        return "".join(res) if len(res) == len(adj) else ""
